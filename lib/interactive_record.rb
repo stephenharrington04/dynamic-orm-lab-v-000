1e90b617ae6cc31a88876ec0bf0df40ba0128bdb
Stephen Harrington <stephenharrington04@yahoo.com>
2018-03-05 16:11:17 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
 
@@ -61,16 +62,19 @@ class InteractiveRecord
     sql = <<-SQL
       SELECT *
       FROM "#{self.table_name}"
-      WHERE name = name
+      WHERE name = ?
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, name)
   end
 
-  def self.find_by(attribute_hash)
-   value = attribute_hash.values.first
-   formatted_value = value.class == Fixnum ? value : "'#{value}'"
-   sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
-   DB[:conn].execute(sql)
+  def self.find_by(attribute)
+    binding.pry
+    sql = <<-SQL
+      SELECT *
+      FROM "#{self.table_name}"
+      WHERE #{attribute.keys.first.to_s} = #{attribute.values.first.to_s}
+    SQL
+    DB[:conn].execute(sql)
   end
 
 end
